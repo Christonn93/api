fc87ac8f1c2975c4375d0e372dd3a49531fc4a53
Christopher Tonnesland <christopher.tonnesland@redgo.no>
2025-02-16 20:40:05 +0100

    Setting up auth

--- src/routes/booking.routes.ts
+++ src/routes/booking.routes.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import { createBooking, getBookings } from "../controllers/booking.controller";
+// import { createBooking, getBookings } from "../controllers/booking.controller";
 
 export const router = express.Router();
 
-router.post("/booking/create", createBooking);
-router.get("/booking/all", getBookings);
-router.get("/booking/:id", getBooking);
+// router.post("/booking/create", createBooking);
+// router.get("/booking/all", getBookings);
+// router.get("/booking/:id", getBooking);
